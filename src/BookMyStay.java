c56a6441accd50ac4467476204d6505d05d516e7
ss8522 <ss8522.srmist.edu.in>
2026-03-16 14:58:37 +0530

    UC2:Basic room types

--- src/BookMyStay.java
+++ src/BookMyStay.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class BookMyStay {
     public static void main(String[] args) {
 
@@ -10,5 +11,51 @@ public class BookMyStay {
 
         System.out.println("Application started successfully!");
         System.out.println("Thank you for using Book My Stay.");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("=================================");
+        System.out.println("Book My Stay App");
+        System.out.println("Hotel Booking System v2.1");
+        System.out.println("=================================");
+
+        // Room availability
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        System.out.println("\nAvailable Room Types:");
+        System.out.println("1. Single Room");
+        System.out.println("2. Double Room");
+        System.out.println("3. Suite Room");
+
+        System.out.print("\nEnter your choice (1-3): ");
+        int choice = sc.nextInt();
+
+        if (choice == 1) {
+            System.out.println("\nSingle Room Details");
+            System.out.println("Beds: 1");
+            System.out.println("Price per Night: $100");
+            System.out.println("Available: " + singleAvailable);
+        }
+        else if (choice == 2) {
+            System.out.println("\nDouble Room Details");
+            System.out.println("Beds: 2");
+            System.out.println("Price per Night: $180");
+            System.out.println("Available: " + doubleAvailable);
+        }
+        else if (choice == 3) {
+            System.out.println("\nSuite Room Details");
+            System.out.println("Beds: 3");
+            System.out.println("Price per Night: $300");
+            System.out.println("Available: " + suiteAvailable);
+        }
+        else {
+            System.out.println("Invalid choice!");
+        }
+
+        sc.close();
+
+
     }
 }
